bf5e185e7f18a12ce4c0f80922c7080dba48ba99
Roshan Choudhary <roshan@byasatech.com>
2026-06-11 12:18:52 +0530

    add key to config file

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://rljfkhiibabxrdnykdxu.supabase.co'
+const supabaseKey ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJsamZraGlpYmFieHJkbnlrZHh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwNDU0NjAsImV4cCI6MjA5NjYyMTQ2MH0.Yjun9HdW9JCmcNAJvR-e74uS1DogDFUYLIC3yzeXGnI'
+
 
 
 export const supabase =
