a95ca01a3626160754570e8bc98291e66e3ba937
Roshan Choudhary <roshan@byasatech.com>
2026-06-14 09:23:53 +0530

    error handling

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/app.css'
 
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+
+    <ToastContainer
+      position="top-right"
+      autoClose={1500}
+      hideProgressBar={false}
+      newestOnTop
+      closeOnClick
+      pauseOnHover
+      theme="colored"
+    />
   </React.StrictMode>
 )
